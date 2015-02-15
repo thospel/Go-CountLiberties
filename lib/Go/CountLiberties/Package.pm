c5951fc2bb838b77c09944836bdddddc7780b5d2
Ton Hospel <git@ton/iguana.be>
2015-02-15 00:17:41 +0000

    Finally drop utils (for once it seems a speedup)

--- lib/Go/CountLiberties/Package.pm
+++ lib/Go/CountLiberties/Package.pm
@@ -8,8 +8,8 @@ use strict;
 use warnings;
 use vars qw($VERSION $SUB_VERSION $release_time %history);
 $VERSION = "1.000";
-$SUB_VERSION = "455";
-$release_time = 1423956030;	## no critic (UselessNoCritic MagicNumbers)
+$SUB_VERSION = "456";
+$release_time = 1423959421;	## no critic (UselessNoCritic MagicNumbers)
 %history = (
   'Changes' => {
     '1.000' => '1.000'
